4aba0e9634299822478feb3d4b9a48628bf3d4d0
Carlos Ligues Orta <carlos@djnr.net>
2019-03-12 17:22:04 -0600

    primera version

---
+++ index.php
@@ -0,0 +1,172 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title>DIESTE 2019 internship program</title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <!-- Place favicon.ico in the root directory -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/mobile.css">
+
+</head>
+
+<body>
+  <!--[if lte IE 9]>
+    <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+  <![endif]-->
+
+
+<div class="wrapper">
+
+  <div class="content">
+      <div class="dieste extra red f18">
+          DIESTE
+      </div>
+      <div class="program extra white f18">
+          2019 internship program
+      </div>
+      <div class="logo">
+        <img src="img/logo.png"/>
+      </div>
+      <div class="darbi extra blue f24">
+        "I’m DARBI, a digital intern programmed to work with the
+        humans at Dieste. I’m in way over my nonexistent head.
+        I don’t understand human emotions because I have none.
+        My ideas are formulaic. I can’t think outside the box.
+        I need your help."
+      </div>
+      <div class="join medium blue f20">
+        Join our team and #upgradeDARBI
+      </div>
+      <div class="padding why extra red f22 under">
+        <div class="info">
+          INFO:
+        </div>
+        <div class="why_dieste">
+          WHY DIESTE ?
+        </div>
+      </div>
+      <div class="padding extra white f16">
+        As a Dieste intern, you get to work with and learn from the most talented real-life creatives in the industry. And, unlike me, you’ll get paid.
+        Join our team and help Dieste save advertising from robotic work like mine.
+      </div>
+      <div class="padding dates">
+        <div class="date extra white f20">PAID INTERNSHIP</div>
+        <div class="date extra white f20">Deadline: 4/10</div>
+        <div class="date extra white f20">Decisions by: 5/1</div>
+        <div class="date extra white f20">Starts: 6/3</div>
+        <div class="date extra white f20">Ends: 8/2</div>
+        <div class="date extra white f20"></div>
+      </div>
+      <div class="positions_header padding extra red f22">
+        POSITIONS
+      </div>
+      <div class="padding positions">
+        <div class="position p1 extra white f16 under">Brand Leadership</div>
+        <div class="position p2 extra white f16 under">Creative</div>
+        <div class="position p3 extra white f16 under">Planning</div>
+        <div class="position p4 extra white f16 under">Production</div>
+        <div class="position p5 extra white f16 under">Media</div>
+        <div class="position p6 extra white f16 under">Public Relations</div>
+      </div>
+      <div class="application extra blue f22 under">
+        APPLICATION:
+      </div>
+
+
+      <form class="padding" action="#" method="post">
+        <div class="step extra blue f22 under">
+          STEP 1:
+        </div>
+        <div class="indication extra white f16">
+          Choose your preferred internship department from favorite to least favorite.
+        </div>
+        <div class="]">
+          <label class="extra white f16" for="choice_1">1st choice*</label>
+          <input type="text" name="choice_1" value="">
+        </div>
+        <div class="">
+          <label class="extra white f16" for="choice_2">2st choice*</label>
+          <input type="text" name="choice_2" value="">
+        </div>
+        <div class="">
+          <label class="extra white f16" for="choice_3">3rd choice*</label>
+          <input type="text" name="choice_3" value="">
+        </div>
+        <div class="step extra blue f22 under">
+          STEP 2:
+        </div>
+        <div class="indication extra white f16">
+          Now provide some information about yourself.
+        </div>
+        <div class="column_one">
+          <label class="extra white f16" for="first_name">First Name*</label>
+          <input type="text" name="first_name" value="">
+        </div>
+        <div class="column_two">
+          <label class="extra white f16" for="last_name">Last Name*</label>
+          <input type="text" name="last_name" value="">
+        </div>
+        <div class="column_one">
+          <label class="extra white f16" for="resume">Upload your resume*</label>
+          <input type="file" name="resume" value="">
+        </div>
+        <div class="column_two">
+          <label class="extra white f16" for="portfolio">Upload your portfolio*</label>
+          <input type="file" name="portfolio" value="">
+        </div>
+        <div class="step extra blue f22 under">
+          STEP 3:
+        </div>
+        <div class="indication extra white f16">
+          What can you do that DARBI can’t?*
+        </div>
+        <div class="column_wide">
+          <textarea name="comments" rows="8" cols="80"></textarea>
+          <div class="submit_button">
+            <input type="submit" id="btn_submit" value="SUBMIT">
+          </div>
+        </div>
+
+      </form>
+      <hr class="line_footer">
+      <footer class="extra white f14 ">
+        <div class="visit">
+          Visit <a href="http://dieste.com">dieste.com</a>
+        </div>
+        <div class="copy">
+          &copy;2019
+        </div>
+      </footer>
+  </div>
+
+
+</div>
+
+
+
+
+
+
+
+
+
+
+  <script src="js/vendor/modernizr-3.6.0.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.3.1.min.js"><\/script>')</script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+
+  <!-- Google Analytics: change UA-XXXXX-Y to be your site's ID. -->
+  <script>
+    window.ga = function () { ga.q.push(arguments) }; ga.q = []; ga.l = +new Date;
+    ga('create', 'UA-XXXXX-Y', 'auto'); ga('send', 'pageview')
+  </script>
+  <script src="https://www.google-analytics.com/analytics.js" async defer></script>
+</body>
+
+</html>
